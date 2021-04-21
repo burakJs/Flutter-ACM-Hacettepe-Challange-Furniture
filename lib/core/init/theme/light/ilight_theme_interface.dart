d986c821806545d1a8e058e88d6a733c7bb5b0b7
your_username <your_email@gmail.com>
2021-04-22 02:53:06 +0300

    Bug fixed

--- lib/core/init/theme/light/ilight_theme_interface.dart
+++ lib/core/init/theme/light/ilight_theme_interface.dart
@@ -1,7 +1,5 @@
 import 'color_scheme_light.dart';
-import 'padding_insets.dart';
 
 abstract class ILightTheme {
   ColorSchemeLight? colorSchemeLight = ColorSchemeLight.instance;
-  PaddingInsetsAll paddingInsetsAll = PaddingInsetsAll();
 }
